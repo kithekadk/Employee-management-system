6e18604b73117b04f030b03f4ce76ebc83d7440b
kithekadk <dankinyi99@gmail.com>
2023-12-08 22:10:53 +0300

    handled merge conflict

--- backend/src/k6_performance/login_user.js
+++ backend/src/k6_performance/login_user.js
@@ -1,37 +1,4 @@
 import http from 'k6/http';
-<<<<<<< HEAD
-import { sleep, check } from 'k6';
-
-export const options ={
-    vus: 1,
-    duration: '1s'
-}
-
-export default function () {
-
-  const body = JSON.stringify({
-    email: 'dankinyi99@gmail.com',
-    password: '12345678'
-  })
-
-  let params = {
-    headers: {
-        'Content-Type': 'application/json'
-    }
-  }
-
-  const response = http.post('http://localhost:4400/employee/login', body, params);
-//   console.log(response);
-
-  check(response,{
-    'is status 200: ': (res)=> res.status == 200,
-    'is successfullt logged in': (res)=> res.body.includes('Logged in successfully')
-  })
-
-  sleep(1);
-}
-
-=======
 import {sleep, check} from 'k6'
 
 export const options = {
@@ -61,4 +28,3 @@ export default function (){
 
     sleep(1)
 }
->>>>>>> 6cfe072809d2eac56d8aeae0043e2e5e2b22c448
